c187fc43603455531b67357add65efc8a3fb45d0
benovice <gkgkdnwls@kw.ac.kr>
2021-07-04 02:39:16 +0900

    Fix bug in Package-lock.json

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "../components/service/login";
+import Login from "../components/service/user/login";
 
 export default function LoginPages(pageProps: any) {
 
