199c3e44f2dd2b92c1194c29a439099de2a1c562
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2021-07-03 16:05:55 +0900

    Add Monaco Editor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Layout } from "../components/layout";
 
-
 export default function Home(pageProps: any) {
   return (
     <React.Fragment>
-      <Layout {...pageProps}>
-      </Layout>
+      <Layout {...pageProps}></Layout>
     </React.Fragment>
-  )
+  );
 }
