b06729d414863c85656f72286b667115aa1ac6b4
Nevation <j209446@naver.com>
2021-07-08 16:20:40 +0900

    Apply graphQL (Close #12)

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,18 @@ import setting from './lib/setting'
 import logging from './lib/logging'
 import log from './module/log'
 import route from './route'
+import graphQLServer from './lib/graphql'
 
 const server = express()
 const Port = 4000
+
 server.set('trust proxy', 1)
 
 server.use(setting)
 server.use(logging)
 server.use('/api', route)
+
+graphQLServer.applyMiddleware({ app: server, path: '/graphql', cors: false })
 
 server.listen(Port, () => {
     console.log(`██████╗░██╗  ░█████╗░░█████╗░██████╗░███████╗  ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░`)
--- server/src/route/test.ts
+++ server/src/route/test.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { ResponseCode } from '../constants/response'
-
 const router = express.Router()
 
 router.get('/test', (_, res) => {
