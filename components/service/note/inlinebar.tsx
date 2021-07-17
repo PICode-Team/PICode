8b885ec1895bc2f6659df596326fa7f020dfc26a
benovice <gkgkdnwls@kw.ac.kr>
2021-07-17 23:58:12 +0900

    Add util drag and drop in text edit page,

--- components/service/note/inlinebar.tsx
+++ components/service/note/inlinebar.tsx
@@ -27,8 +27,9 @@ export default function TestNote() {
     const [test, setTest] = React.useState<INoteContent[]>([]);
     const [show, setShow] = React.useState<boolean>(false);
     const [position, setPosition] = React.useState<IPosition>({ x: 0, y: 0, target: 0 });
-    const [highlight, setHighlight] = React.useState<number>(0);
+    const [highlight, setHighlight] = React.useState<number>();
     const [drag, setDrag] = React.useState("");
+    const [dragEnd, setDragEnd] = React.useState(false);
 
     let tmpPosition: any = [];
 
@@ -38,8 +39,18 @@ export default function TestNote() {
     }, [cursor])
 
     useEffect(() => {
-
-    }, [test])
+        if (!dragEnd) return;
+        let tmpContent = cloneDeep(test)
+        for (let i in test) {
+            let node = document.getElementById(`${i}`)
+            if (node) {
+                node.innerText = test[i].text
+            }
+        }
+        setHighlight(undefined)
+        setDragEnd(false)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dragEnd])
 
     return <div className={classes.root}>
         <div className={classes.fileView}>
@@ -61,7 +72,10 @@ export default function TestNote() {
                     if (show) {
                         setShow(false)
                     }
-                    if (tmpContent[tmpContent.length - 1]?.text === "") return;
+                    if (tmpContent[tmpContent.length - 1]?.text === "") {
+                        document.getElementById(String(tmpContent.length - 1))?.focus();
+                        return;
+                    }
                     tmpContent.push({
                         text: ""
                     })
@@ -93,7 +107,9 @@ export default function TestNote() {
                     </div>}
                     {test.map((v: INoteContent, idx: number) => {
                         return <div key={idx} style={{ height: "fit-content", width: "100%", position: "relative" }}
-                            onClick={(e) => { e.stopPropagation(); }}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                            }}
                             className={clsx(v.clicked && classes.clicked)}
                             onMouseOver={() => {
                                 let tool = document.getElementById(`${idx}tool`);
@@ -102,7 +118,55 @@ export default function TestNote() {
                             onMouseOut={() => {
                                 let tool = document.getElementById(`${idx}tool`);
                                 if (tool) { tool.style.visibility = "hidden"; }
-                            }}>
+                            }}
+                            onDragStart={(e) => {
+                                setDrag(e.currentTarget.id)
+                                for (let i in test) {
+                                    let node = document.getElementById(`${i}`);
+                                    if (node) {
+                                        tmpPosition.push(node.getBoundingClientRect().bottom)
+                                    }
+                                }
+                            }}
+                            onMouseUpCapture={(e) => {
+                                let nodePosition = document.getElementById(`${idx}`)
+                                let tmpContent = cloneDeep(test);
+                                if (highlight !== undefined) {
+                                    if (tmpContent[highlight]) {
+                                        tmpContent[highlight].clicked = false
+                                    }
+                                }
+                                if (window && nodePosition) {
+                                    if (
+                                        window.getSelection()?.toString() === v.text &&
+                                        e.clientX < nodePosition.getBoundingClientRect().left
+                                    ) {
+                                        tmpContent[idx].clicked = true;
+                                        setHighlight(idx);
+                                    }
+                                }
+                                setTest(tmpContent)
+                            }}
+                            onDragEnd={(e) => {
+                                let tmpContent = cloneDeep(test);
+                                let tmpNode: any = tmpContent.splice(Number(drag), 1)
+                                let lastCheck = true;
+                                for (let i in tmpPosition) {
+                                    if (tmpPosition[i] > e.clientY) {
+                                        tmpContent.splice(Number(i) - 1, 0, tmpNode[0])
+                                        setTest(tmpContent)
+                                        lastCheck = false;
+                                        setDragEnd(true)
+                                        return;
+                                    }
+                                }
+                                if (lastCheck) {
+                                    tmpContent = tmpContent.concat(tmpNode)
+                                    setTest(tmpContent)
+                                    setDragEnd(true)
+                                }
+                            }}
+                        >
                             <div className={classes.leftTool} id={`${idx}tool`}>
                                 <IconButton style={{ float: "left", width: "20px", height: "20px" }}
                                     className={classes.mouseOver}
@@ -110,57 +174,48 @@ export default function TestNote() {
                                         let tmpContent = cloneDeep(test);
                                         tmpContent[idx].clicked = true;
                                         setTest(tmpContent)
-                                    }} >
+                                    }}
+                                    onMouseDown={(e) => {
+                                        console.log(e)
+                                    }}
+                                >
                                     <DragIndicatorIcon style={{ width: "20px", height: "20px" }} />
                                 </IconButton>
-                                <IconButton style={{ float: "left", width: "20px", height: "20px" }} onClick={(e) => {
-                                    let tool = document.getElementById(`${idx}tool`);
-                                    if (tool) {
-                                        setPosition({ x: tool.getBoundingClientRect().left - 10, y: tool.getBoundingClientRect().top - 50, target: idx })
-                                        setShow(true)
-                                    }
+                                <IconButton style={{ float: "left", width: "20px", height: "20px" }}
+                                    onClick={(e) => {
+                                        let tool = document.getElementById(`${idx}tool`);
+                                        if (tool) {
+                                            setPosition({ x: tool.getBoundingClientRect().left - 10, y: tool.getBoundingClientRect().top - 50, target: idx })
+                                            setShow(true)
+                                        }
 
-                                }}>
+                                    }}>
                                     <AddIcon style={{ width: "20px", height: "20px" }} />
                                 </IconButton>
                             </div>
                             <div className={classes.write}>
                                 <div
+                                    draggable={false}
                                     className={clsx(classes.defaultInput, v.type !== undefined && classes[v.type])}
                                     id={String(idx)}
-                                    onDragStart={(e) => {
-                                        console.log(1)
-                                        setDrag(e.currentTarget.id)
-                                        for (let i in test) {
-                                            let node = document.getElementById(`${i}`);
-                                            if (node) {
-                                                console.log(test)
-                                                tmpPosition.push(node.getBoundingClientRect().bottom)
-                                            }
-                                        }
-                                        console.log(tmpPosition)
-                                        console.log(2)
-                                    }}
-                                    onDragEnd={(e) => {
-                                        console.log(3)
-                                        let tmpContent = cloneDeep(test);
-                                        let tmpNode: any = tmpContent.splice(Number(drag), 1)
-                                        console.log(tmpPosition)
-                                        for (let i in tmpPosition) {
-                                            console.log(tmpPosition[i], e.clientY)
-                                            if (tmpPosition[i] > e.clientY) {
-                                                tmpContent.splice(Number(i) - 1, 0, tmpNode)
-                                                setTest(tmpContent)
-                                                return;
-                                            }
-                                        }
-                                        console.log(tmpContent)
-                                        console.log(4)
+                                    onDrop={(e) => {
+                                        e.preventDefault()
+                                        return false;
                                     }}
                                     contentEditable={true}
-                                    placeholder="Plz Input Text"
+                                    onSelect={(e) => {
+                                        let node = document.getElementById(`${idx}`)
+                                        if (node) {
+                                            node?.setAttribute("placeholder", "Plz Input Text")
+                                        }
+                                    }}
+                                    onBlur={(e) => {
+                                        let node = document.getElementById(`${idx}`)
+                                        if (node) {
+                                            node?.setAttribute("placeholder", "")
+                                        }
+                                    }}
                                     onInput={(e) => {
-                                        console.log(e.currentTarget.textContent)
                                         let tmpContent = cloneDeep(test);
                                         tmpContent[idx].text = e.currentTarget.textContent ?? ""
                                         setTest(tmpContent)
