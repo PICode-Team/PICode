43b72d8ef2023dea1bfe9a8b3f16bd0037f00c95
Nevation <j209446@naver.com>
2021-07-18 22:53:50 +0900

    create chatting socket (Close #20)

--- server/src/module/data/chatManager.ts
+++ server/src/module/data/chatManager.ts
@@ -1,12 +1,46 @@
 import { DataDirectoryPath } from "../../types/module/data/data.types";
 import fs from 'fs'
-import { TChatChannelData, TChatType } from "../../types/module/data/chat.types";
+import { TChatChannelData, TChatLogData, TChatLogDataParam, TChatType } from "../../types/module/data/chat.types";
 import { getJsonData, setJsonData } from "./fileManager";
+import { getTime } from "../datetime";
 
+
+const ChatQueue: TChatLogDataParam[] = []
 
 export default class DataChatManager {
-    static getChatDataPath({ type, chatName, fileName, userId }: { chatName?: string, type?: TChatType, fileName?: 'channelInfo.json', userId?: string } = {}) {
-        return type === undefined ? `${DataDirectoryPath}/chat` : type === 'channel' ? `${DataDirectoryPath}/chat/#${chatName}/${fileName}` : `${DataDirectoryPath}/chat/${userId}/${chatName}/${fileName}`
+    static async run() {
+        setInterval(() => {
+            const time = getTime()
+            const originData = ChatQueue.shift()
+
+            if (originData === undefined) {
+                return
+            }
+
+            const chatObject = { ...originData, time } as Required<TChatLogDataParam>
+            const chatLogFileName = getTime(time, 'YYYY-MM-DD HH')
+            const chatDir = `${this.getChatDataPath()}/${this.getDirName(chatObject.sender!, chatObject.chatName!)}/log`
+            const chatLogFilePath = `${chatDir}/${chatLogFileName}`
+
+            if (!fs.existsSync(chatDir)) {
+                fs.mkdirSync(chatDir, { recursive: true })
+            }
+
+            const chatLogData: TChatLogData[] = (() => {
+                const saveObject = { ...chatObject, chatName: undefined }
+                if (!fs.existsSync(chatLogFilePath)) {
+                    return [saveObject]
+                } else {
+                    return [...getJsonData(chatLogFilePath), saveObject]
+                }
+            })()
+
+            setJsonData(chatLogFilePath, chatLogData)
+        }, 100)
+    }
+
+    static getChatDataPath() {
+        return `${DataDirectoryPath}/chat`
     }
 
     static getChatType(chatName: string): TChatType {
@@ -18,25 +52,59 @@ export default class DataChatManager {
         return fs.existsSync(this.getChatDataPath() + `/${chatName}`)
     }
 
-    static getChat(userId: string, chatName?: string) {
-        const directMessageList = fs.readdirSync(this.getChatDataPath() + `/${userId}`)
-        const channelMessageList = fs.readdirSync(this.getChatDataPath()).filter(v => this.getChatType(v) === 'channel').filter(v => {
-            const channelInfoData = getJsonData(this.getChatDataPath({ type: 'channel', fileName: 'channelInfo.json', chatName: v })) as TChatChannelData
-            return channelInfoData.chatParticipant.indexOf(userId) > -1
-        }).map(v => `#${v}`)
 
-        return [...directMessageList, ...channelMessageList].filter(v => chatName === undefined || v === chatName)
+    static getChat(userId: string, chatName?: string) {
+        const chatDefaultPath = this.getChatDataPath()
+        const chattingList = fs.readdirSync(chatDefaultPath)
+
+        const nameFilter = (name: string) => {
+            if (chatName === undefined) {
+                return true
+            }
+
+            const type = this.getChatType(name)
+
+            return (type === 'channel' && name === chatName) || (type === 'direct' && name === this.getDirectMessageDirName(userId, chatName))
+        }
+
+        return chattingList.filter(v => nameFilter(v)).map(v => {
+            return getJsonData(`${this.getChatDataPath()}/${v}/chatInfo.json`) as TChatChannelData
+        }).filter(v => v.chatParticipant.indexOf(userId) > -1)
+
     }
 
-    // static getChatContent(userId: string, chatName: string) {
+    static saveChat(sender: string, chatName: string, message: string) {
+        ChatQueue.push({ sender, chatName, message })
+    }
 
-    // }
+    static getChatLogList(userId: string, chatName: string) {
+        return fs.readdirSync(`${this.getChatDataPath()}/${this.getDirName(userId, chatName)}/log`)
+    }
+
+    static getChatLog(userId: string, chatName: string, page: string) {
+        const dataPath = `${this.getChatDataPath()}/${this.getDirName(userId, chatName)}/log/${page}`
+
+        if (!fs.existsSync(dataPath)) {
+            return []
+        }
+        return getJsonData(dataPath) as TChatLogData[]
+    }
+
+    static getDirName(userId: string, chatName: string) {
+        return this.getChatType(chatName) === 'channel' ? chatName : this.getDirectMessageDirName(userId, chatName)
+    }
+
+    static getDirectMessageDirName(userId1: string, userId2: string) {
+        return [`@${userId1}`, userId2].sort().join('{sep}')
+    }
 
     static createChannel(channelData: TChatChannelData) {
-        if (this.isExistsChannel(channelData.channelName)) {
+        if (this.isExistsChannel(channelData.chatName)) {
             return false
         }
 
-        return setJsonData(this.getChatDataPath({ chatName: channelData.channelName, type: 'channel', fileName: 'channelInfo.json' }), channelData)
+        return setJsonData(
+            `${this.getChatDataPath()}/${this.getDirName(channelData.userId, channelData.chatName)}/chatInfo.json`, { ...channelData, creation: getTime() }
+        )
     }
 }
--- server/src/module/socket/chat.ts
+++ server/src/module/socket/chat.ts
@@ -1,9 +1,11 @@
 import { TSocketPacket } from "../../types/module/socket.types";
+import DataChatManager from "../data/chatManager";
 import { getSocket, makePacket } from "./manager";
 
 
 function sendMessage(sender: string, target: string, message: string) {
     const sendData = JSON.stringify(makePacket('chat', 'sendMessage', { message, sender }))
+    DataChatManager.saveChat(sender, target, message)
 
     if (sender !== target) {
         getSocket(sender)?.send(sendData)
@@ -12,10 +14,30 @@ function sendMessage(sender: string, target: string, message: string) {
     getSocket(target)?.send(sendData)
 }
 
-// function createChannel(creator: string, channelName: string, data: {}) {
+function createChannel(creator: string, chatName: string, data: {}) {
+    DataChatManager.createChannel({
+        userId: creator,
+        chatName,
+        description: '',
+        chatParticipant: [creator]
+    })
+}
 
-// }
+function getChat(userId: string) {
+    const sendData = JSON.stringify(makePacket('chat', 'getChat', DataChatManager.getChat(userId)))
+    getSocket(userId).send(sendData)
+}
 
+
+function getChatLog(userId: string, chatName: string, page: string) {
+    const sendData = JSON.stringify(makePacket('chat', 'getChatLog', DataChatManager.getChatLog(userId, chatName, page)))
+    getSocket(userId).send(sendData)
+}
+
+function getChatLogList(userId: string, chatName: string) {
+    const sendData = JSON.stringify(makePacket('chat', 'getChatLogList', DataChatManager.getChatLogList(userId, chatName)))
+    getSocket(userId).send(sendData)
+}
 
 
 export default function chat(userId: string, packet: TSocketPacket) {
@@ -26,6 +48,19 @@ export default function chat(userId: string, packet: TSocketPacket) {
             break
         }
         case 'createChannel': {
+            createChannel(userId, packet.data.target, packet.data)
+            break
+        }
+        case 'getChat': {
+            getChat(userId)
+            break
+        }
+        case 'getChatLog': {
+            getChatLog(userId, packet.data.target, packet.data.page)
+            break
+        }
+        case 'getChatLogList': {
+            getChatLogList(userId, packet.data.taret)
             break
         }
     }
