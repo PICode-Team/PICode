0757518d891da8e66716f7c9192155f559939225
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2021-07-18 14:54:46 +0900

    Add Code Redux

--- modules/index.ts
+++ modules/index.ts
@@ -3,32 +3,36 @@ import { HYDRATE } from "next-redux-wrapper";
 import theme, { IThemeState } from "./theme";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import code from "./code";
+import { TEditorRoot } from "../components/service/code/types";
 
 const persistConfig = {
-    key: "root",
-    storage,
-    whiteList: [""],
+  key: "root",
+  storage,
+  whiteList: [""],
 };
 
 const rootReducer = (
-    state: IState | undefined,
-    action: AnyAction
+  state: IState | undefined,
+  action: AnyAction
 ): CombinedState<IState> => {
-    switch (action.type) {
-        case HYDRATE:
-            return { ...state, ...action.payload.theme };
-        default: {
-            const combineReducer = combineReducers({
-                theme,
-            });
-            return combineReducer(state, action);
-        }
+  switch (action.type) {
+    case HYDRATE:
+      return { ...state, ...action.payload.theme };
+    default: {
+      const combineReducer = combineReducers({
+        theme,
+        code,
+      });
+      return combineReducer(state, action);
     }
+  }
 };
 
 export type RootState = ReturnType<typeof rootReducer>;
 export default persistReducer(persistConfig, rootReducer);
 
 interface IState {
-    theme: IThemeState;
+  theme: IThemeState;
+  code: TEditorRoot;
 }
