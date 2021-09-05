293e3a520688b6a33933dbd7f215f1272c27c25c
jingu <wlsrn3684@naver.com>
2021-09-05 15:37:41 +0900

    Redesign alarm view

--- components/layout/item/alert.tsx
+++ components/layout/item/alert.tsx
@@ -1,25 +1,70 @@
 import { IconButton } from "@material-ui/core";
-import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
+import { Cancel, Clear } from "@material-ui/icons";
+import ClearRoundedIcon from "@material-ui/icons/ClearRounded";
 import React from "react";
 import { alertDialogStyle } from "../../../styles/layout/item/alert";
 
 export default function AlertDialog(props: any) {
-    const classes = alertDialogStyle();
-    return <div className={classes.content}>
-        <div className={classes.header}>
-            Recent Alarm
-            <IconButton onClick={() => {
-                props.setOpenAlert(false);
-            }}>
-                <ClearRoundedIcon />
-            </IconButton>
-        </div>
-        <div className={classes.alarmContent}>
-            {props.data !== undefined && props.data.map((v: any) => {
-                return <div key={v.content} style={{ borderBottom: "1px solid #fff", borderTop: "1px solid #fff" }}>
-                    {v.content}
+  const classes = alertDialogStyle();
+  return (
+    <div className={classes.content}>
+      <div className={classes.header}>Recent Alarm</div>
+      <div className={classes.alarmContent}>
+        {props.data !== undefined &&
+          props.data.map((v: any, i: number) => {
+            return (
+              <div
+                key={`alarm-${i}`}
+                style={{
+                  color: "#ffffff",
+                  padding: "6px 14px",
+                  backgroundColor: "#505965",
+                  margin: "0px -10px",
+                  display: "flex",
+                  fontSize: "12px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                  }}
+                >
+                  <div>
+                    <div
+                      style={{
+                        width: "32px",
+                        height: "32px",
+                        backgroundColor: "#ffffff",
+                        borderRadius: "16px",
+                        marginRight: "6px",
+                      }}
+                    ></div>
+                  </div>
+                  <div>
+                    <div
+                      style={{ fontWeight: "bold" }}
+                    >{`${v.userId}'s ${v.type}`}</div>
+                    <div>{v.content}</div>
+                  </div>
                 </div>
-            })}
-        </div>
+                <div
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick={(e: any) => {}}
+                >
+                  <Clear
+                    style={{
+                      width: "18px",
+                      height: "18px",
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
+  );
 }
--- components/layout/topbar.tsx
+++ components/layout/topbar.tsx
@@ -97,6 +97,17 @@ export function Topbar(ctx: any) {
     }
   }, [ctx]);
 
+  let uuidv4 = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  };
+
   const makeUserInfo = (data: IUserWorkInfo[]) => {
     let returnData = [];
     let idx = 0;
@@ -107,6 +118,7 @@ export function Topbar(ctx: any) {
       if (i.userId !== ctx.session.userId) {
         returnData.push(
           <div
+            key={uuidv4()}
             className={classes.userInfoData}
             style={{ zIndex: idx }}
             onClick={() => {
